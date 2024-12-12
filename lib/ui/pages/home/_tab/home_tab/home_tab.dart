da4f2522f94cf4b950bca1bcb7e5c1b0cd42ce80
wd247 <junny3344@gmail.com>
2024-12-12 19:59:14 +0900

    edit wish sales purch, move file

--- lib/ui/pages/home/_tab/home_tab/home_tab.dart
+++ lib/ui/pages/home/_tab/home_tab/home_tab.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_market_app/ui/pages/home/_tab/home_tab/widgets/home_tab_app_bar.dart';
-import 'package:flutter_market_app/ui/pages/home/_tab/home_tab/widgets/home_tab_list_view.dart';
+import 'package:flutter_market_app/ui/widgets/home_tab_list_view.dart';
 
 class HomeTab extends StatelessWidget {
   @override
