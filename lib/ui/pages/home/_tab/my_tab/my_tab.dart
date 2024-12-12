d77aa0eea1cacf65324d1a291c09f13612e0f99e
oosuhada <woosu91@gmail.com>
2024-12-12 19:30:02 +0900

    add dark mode theme

--- lib/ui/pages/home/_tab/my_tab/my_tab.dart
+++ lib/ui/pages/home/_tab/my_tab/my_tab.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_market_app/core/snackbar_util.dart';
 import 'package:flutter_market_app/ui/pages/home/_tab/my_tab/widgets/my_profile_box.dart';
 import 'package:flutter_market_app/ui/pages/home/_tab/my_tab/widgets/my_tab_app_bar.dart';
 
@@ -17,18 +16,52 @@ class MyTab extends StatelessWidget {
               MyProfileBox(),
               SizedBox(height: 12),
               label('나의 거래'),
-              item(text: '관심목록', icon: CupertinoIcons.heart),
-              item(text: '판매내역', icon: CupertinoIcons.square_list),
-              item(text: '구매내역', icon: CupertinoIcons.bag),
-              item(text: '거래 가계부', icon: CupertinoIcons.book),
+              item(
+                  context: context,
+                  text: '관심목록',
+                  icon: CupertinoIcons.heart,
+                  route: '/wishlist'),
+              item(
+                  context: context,
+                  text: '판매내역',
+                  icon: CupertinoIcons.square_list,
+                  route: '/sales_history'),
+              item(
+                  context: context,
+                  text: '구매내역',
+                  icon: CupertinoIcons.bag,
+                  route: '/purchase_history'),
+              item(
+                  context: context,
+                  text: '거래 가계부',
+                  icon: CupertinoIcons.book,
+                  route: '/transaction_account'),
               Divider(),
               label('환경설정'),
-              item(text: '관심 도시 변경', icon: Icons.location_on_outlined),
-              item(text: '언어 변경', icon: Icons.language_outlined),
-              item(text: '통화 변경', icon: Icons.currency_exchange),
+              item(
+                  context: context,
+                  text: '관심 도시 변경',
+                  icon: Icons.location_on_outlined,
+                  route: '/city_selection'),
+              item(
+                  context: context,
+                  text: '언어 변경',
+                  icon: Icons.language_outlined,
+                  route: '/language_selection'),
+              item(
+                  context: context,
+                  text: '통화 변경',
+                  icon: Icons.currency_exchange,
+                  route: '/currency_selection'),
               Divider(),
-              item(text: '자주 묻는 질문'),
-              item(text: '약관 및 정책'),
+              item(
+                  context: context,
+                  text: '자주 묻는 질문',
+                  route: '/frequently_asked_questions'),
+              item(
+                  context: context,
+                  text: '약관 및 정책',
+                  route: '/terms_and_policies'),
             ],
           ),
         ),
@@ -36,7 +69,6 @@ class MyTab extends StatelessWidget {
     );
   }
 
-  // label과 item 위젯은 그대로 유지
   Widget label(String text) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
@@ -51,33 +83,35 @@ class MyTab extends StatelessWidget {
   }
 
   Widget item({
+    required BuildContext context,
     required String text,
     IconData? icon,
+    required String route,
   }) {
-    return Builder(builder: (context) {
-      return GestureDetector(
-        onTap: () {
-          SnackbarUtil.showSnackBar(context, '준비중 입니다');
-        },
-        child: Container(
-          height: 40,
-          color: Colors.transparent,
-          child: Row(
-            children: [
-              if (icon != null) ...[
-                Icon(icon),
-                SizedBox(width: 8),
-              ],
-              Text(
-                text,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
+    return GestureDetector(
+      onTap: () => navigateToPage(context, route),
+      child: Container(
+        height: 40,
+        color: Colors.transparent,
+        child: Row(
+          children: [
+            if (icon != null) ...[
+              Icon(icon),
+              SizedBox(width: 8),
             ],
-          ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
-      );
-    });
+      ),
+    );
+  }
+
+  void navigateToPage(BuildContext context, String route) {
+    Navigator.pushNamed(context, route);
   }
 }
