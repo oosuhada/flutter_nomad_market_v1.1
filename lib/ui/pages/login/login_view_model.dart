1502369dcf2507e1bea3cd6f2325169eb41cab97
김시은 <gimsieun@gimsieun-ui-MacBookAir.local>
2024-12-13 16:21:04 +0900

    [feature/login_status] modify login_page and login_view_model

--- lib/ui/pages/login/login_view_model.dart
+++ lib/ui/pages/login/login_view_model.dart
@@ -1,18 +1,17 @@
 // 1. 상태클래스 만들기 => X
 
 // 2. 뷰모델 만들기
-
-import 'package:flutter_market_app/data/repository/user_repository.dart';
+import 'package:flutter_market_app/data/repository/user_info_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginViewModel {
-  Future<bool> login({
-    required String username,
+  final userInfoRepository = UserInfoRepository();
+  Future<bool?> login({
+    required String email,
     required String password,
   }) async {
-    final userRepository = UserRepository();
-    return await userRepository.login(
-      username: username,
+    return await userInfoRepository.login(
+      email: email,
       password: password,
     );
   }
