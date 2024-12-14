a382e99a3ad18307c86bd573fa6e80ae9c3d5c82
oosuhada <woosu91@gmail.com>
2024-12-14 12:41:47 +0900

    add design file

---
+++ lib/app_theme.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme() {
+    return ThemeData(
+      brightness: Brightness.light,
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple.shade900),
+      highlightColor: Colors.purple.shade900,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 254, 248, 245),
+      appBarTheme: const AppBarTheme(
+        centerTitle: false,
+        backgroundColor: Color.fromARGB(255, 254, 248, 245),
+        titleTextStyle: TextStyle(
+          fontSize: 18,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(
+              const Color.fromARGB(255, 254, 248, 245)),
+          backgroundColor: MaterialStateProperty.all(Colors.purple.shade900),
+          minimumSize: MaterialStateProperty.all(const Size.fromHeight(52)),
+          textStyle: MaterialStateProperty.all(
+            const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.grey,
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 20,
+        ),
+        border: MaterialStateOutlineInputBorder.resolveWith((states) {
+          if (states.contains(MaterialState.focused)) {
+            return OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: Colors.grey,
+                width: 2,
+              ),
+            );
+          }
+          if (states.contains(MaterialState.error)) {
+            return OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                color: Colors.red[100]!,
+                width: 2,
+              ),
+            );
+          }
+          return OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: Colors.grey[300]!,
+              width: 1,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  static ThemeData darkTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.purple.shade900,
+        brightness: Brightness.dark,
+      ),
+      highlightColor: Colors.purple.shade900,
+      scaffoldBackgroundColor: Colors.grey[900],
+      appBarTheme: AppBarTheme(
+        centerTitle: false,
+        backgroundColor: Colors.grey[900],
+        titleTextStyle: const TextStyle(
+          fontSize: 18,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          backgroundColor: MaterialStateProperty.all(Colors.purple.shade900),
+          minimumSize: MaterialStateProperty.all(const Size.fromHeight(52)),
+          textStyle: MaterialStateProperty.all(
+            const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          shadowColor: MaterialStateProperty.all(Colors.black.withOpacity(0.5)),
+          elevation: MaterialStateProperty.all(8),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.grey[400],
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 20,
+        ),
+        border: MaterialStateOutlineInputBorder.resolveWith((states) {
+          if (states.contains(MaterialState.focused)) {
+            return OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                color: Colors.grey[400]!,
+                width: 2,
+              ),
+            );
+          }
+          if (states.contains(MaterialState.error)) {
+            return OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                color: Colors.red[300]!,
+                width: 2,
+              ),
+            );
+          }
+          return OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: Colors.grey[600]!,
+              width: 1,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
